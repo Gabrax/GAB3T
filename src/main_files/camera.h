70eb2f1244736cf46f129755957c1940e9c8e91b
Gabrax <gab-oz@wp.pl>
2024-04-11 19:45:16 +0200

    ignore debug and release

--- src/main_files/camera.h
+++ src/main_files/camera.h
@@ -6,7 +6,7 @@
 
 #include <vector>
 
-// Defines several possible options for camera movement. Used as abstraction to stay away from window-system specific input methods
+
 enum Camera_Movement {
     FORWARD,
     BACKWARD,
@@ -26,7 +26,7 @@ const float ZOOM = 45.0f;
 
 
 
-// An abstract camera class that processes input and calculates the corresponding Euler Angles, Vectors and Matrices for use in OpenGL
+
 class Camera
 {
 public:
@@ -71,7 +71,7 @@ public:
         return glm::lookAt(Position, Position + Front, Up);
     }
 
-    // processes input received from any keyboard-like input system. Accepts input parameter in the form of camera defined ENUM (to abstract it from windowing systems)
+    // processes input received from any keyboard-like input system. 
     void ProcessKeyboard(Camera_Movement direction, float deltaTime)
     {
         float velocity = MovementSpeed * deltaTime;
